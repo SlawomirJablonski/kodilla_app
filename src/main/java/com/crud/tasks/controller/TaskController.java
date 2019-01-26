3421c8ecdb09ca517f4aa66c7961c70e512be257
SlawomirJablonski <forest19@interia.pl>
2019-01-26 21:23:07 +0100

    New Request Mapping Shortcuts for TaskController methods

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,10 +4,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,28 +17,28 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @GetMapping("getTasks")
     public List<TaskDto> getTasks(){
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    @GetMapping("getTask")
     public TaskDto getTask(Long taskId){
         return new TaskDto(1L,"test title","test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    @DeleteMapping("deleteTask")
     public void deleteTask(Long taskId){
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-    public TaskDto updateTask(@RequestParam("taskDto") TaskDto taskDto){
+    @PutMapping("updateTask")
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
         return new TaskDto(1L,"Edited test title","test content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask")
-    public void createTask(@RequestParam("taskDto") TaskDto taskDto){
+    @PostMapping("createTask")
+    public void createTask(@RequestBody TaskDto taskDto){
 
     }
 }
